c83ef38abdb9508851c33e505b2aabf51ba59a47
wooyounggggg <souljit2@gmail.com>
2022-10-18 22:44:47 +0900

    init: 기본 세팅

---
+++ src/main/java/com/depromeet/coquality/CoQualityApplication.java
@@ -0,0 +1,13 @@
+package com.depromeet.coquality;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoQualityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoQualityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/depromeet/coquality/CoQualityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.depromeet.coquality;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoQualityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
