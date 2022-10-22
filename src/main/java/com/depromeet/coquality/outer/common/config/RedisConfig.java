0fd4a33b5dbae956edbea39f331facf3c684e0aa
wooyounggggg <souljit2@gmail.com>
2022-10-22 23:24:24 +0900

    feat: Post Redis Adapter 구현

---
+++ src/main/java/com/depromeet/coquality/outer/common/config/RedisConfig.java
@@ -0,0 +1,52 @@
+package com.depromeet.coquality.outer.common.config;
+
+import com.depromeet.coquality.outer.post.entity.RedisPost;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String host;
+
+    @Value("${spring.redis.port}")
+    private int port;
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory(host, port);
+    }
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS); // timestamp 형식 안따르도록 설정
+        mapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        mapper.registerModules(new JavaTimeModule(),
+            new Jdk8Module()); // LocalDateTime 매핑을 위해 모듈 활성화
+        return mapper;
+    }
+
+    @Bean
+    public RedisTemplate<String, RedisPost> redisTemplate() {
+        var redisTemplate = new RedisTemplate<String, RedisPost>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer(objectMapper()));
+
+        return redisTemplate;
+    }
+}
---
+++ src/main/java/com/depromeet/coquality/outer/post/adapter/driven/persistence/RedisInsertPostAdapter.java
@@ -0,0 +1,31 @@
+package com.depromeet.coquality.outer.post.adapter.driven.persistence;
+
+import com.depromeet.coquality.inner.post.domain.Post;
+import com.depromeet.coquality.inner.post.port.driven.InsertPostPort;
+import com.depromeet.coquality.outer.post.entity.RedisPost;
+import java.time.Duration;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class RedisInsertPostAdapter implements InsertPostPort {
+
+    private final RedisTemplate<Integer, RedisPost> redisTemplate;
+
+    @Override
+    public void insert(Post post) {
+        final var redisPost = RedisPost.factory()
+            .title(post.getTitle())
+            .newInstance();
+
+        redisTemplate
+            .opsForValue()
+            .setIfAbsent(
+                post.hashCode(),
+                redisPost,
+                Duration.ofSeconds(2)
+            );
+    }
+}
