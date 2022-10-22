9e1b3053310a9a77241e52816a4e27a4d310b71c
wooyounggggg <souljit2@gmail.com>
2022-10-22 23:24:56 +0900

    feat: Common domain constant

---
+++ src/main/java/com/depromeet/coquality/inner/common/domain/constant/CommonConstant.java
@@ -0,0 +1,8 @@
+package com.depromeet.coquality.inner.common.domain.constant;
+
+public class CommonConstant {
+
+    public static final String MODIFY = "수정";
+    public static final String DELETE = "삭제";
+
+}
