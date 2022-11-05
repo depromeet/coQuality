395d83bd38501d2b076fd0e0b525714b442721d1
LeeJejune <leejejune0625@gmail.com>
2022-11-05 18:16:06 +0900

    feat: @FeignClient 통해 카카오 유저정보 조회

---
+++ src/main/java/com/depromeet/coquality/outer/user/external/client/kakao/dto/response/KakaoAccount.java
@@ -0,0 +1,17 @@
+package com.depromeet.coquality.outer.user.external.client.kakao.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@JsonIgnoreProperties(ignoreUnknown = true)
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+public class KakaoAccount {
+    @JsonProperty("email")
+    private String email;
+}
