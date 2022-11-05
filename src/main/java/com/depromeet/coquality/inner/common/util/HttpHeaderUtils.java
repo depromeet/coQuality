baae24e61bc9f5af29d3b387a194c61086e7e417
LeeJejune <leejejune0625@gmail.com>
2022-11-05 18:18:49 +0900

    feat: 카카오 회원가입 Service 구현

---
+++ src/main/java/com/depromeet/coquality/inner/common/util/HttpHeaderUtils.java
@@ -0,0 +1,13 @@
+package com.depromeet.coquality.inner.common.util;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class HttpHeaderUtils {
+    private static final String BEARER_TOKEN = "Bearer ";
+
+    public static String withBearerToken(String token) {
+        return BEARER_TOKEN.concat(token);
+    }
+}
---
+++ src/main/java/com/depromeet/coquality/inner/user/apllication/service/kakao/KaKaoSignUpService.java
@@ -0,0 +1,27 @@
+package com.depromeet.coquality.inner.user.apllication.service.kakao;
+
+import com.depromeet.coquality.inner.common.util.HttpHeaderUtils;
+import com.depromeet.coquality.inner.user.domain.User;
+import com.depromeet.coquality.inner.user.port.driven.UserPort;
+import com.depromeet.coquality.inner.user.port.driving.SignUpUserUseCase;
+import com.depromeet.coquality.inner.user.port.driving.dto.request.SignUpDto;
+import com.depromeet.coquality.outer.user.entity.UserSocialType;
+import com.depromeet.coquality.outer.user.external.client.kakao.KaKaoAuthApiClient;
+import com.depromeet.coquality.outer.user.external.client.kakao.dto.response.KaKaoProfileResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KaKaoSignUpService implements SignUpUserUseCase {
+
+    private static final UserSocialType socialType = UserSocialType.KAKAO;
+    private final UserPort userPort;
+    private final KaKaoAuthApiClient kakaoAuthApiCaller;
+
+    @Override
+    public Long execute(final SignUpDto request) {
+        final KaKaoProfileResponse response = kakaoAuthApiCaller.getProfileInfo(HttpHeaderUtils.withBearerToken(request.getToken()));
+        return userPort.insert(User.of(response.getId(), response.getAccount().getEmail(), String.valueOf(socialType)));
+    }
+}
