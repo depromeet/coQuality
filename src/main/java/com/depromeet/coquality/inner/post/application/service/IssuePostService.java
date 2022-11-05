57fa4f75ae292be0af75c128bd1b31729ef3c171
wooyounggggg <souljit2@gmail.com>
2022-11-06 01:51:25 +0900

    feat: PostIssueService 개발

--- src/main/java/com/depromeet/coquality/inner/post/application/service/IssuePostService.java
+++ src/main/java/com/depromeet/coquality/inner/post/application/service/IssuePostService.java
@@ -2,18 +2,18 @@ package com.depromeet.coquality.inner.post.application.service;
 
 import com.depromeet.coquality.inner.post.domain.Post;
 import com.depromeet.coquality.inner.post.port.driven.PostPort;
-import com.depromeet.coquality.inner.post.port.driving.CreatePostUseCase;
+import com.depromeet.coquality.inner.post.port.driving.IssuePostUseCase;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CreatePostService implements CreatePostUseCase {
+public class IssuePostService implements IssuePostUseCase {
 
     private final PostPort postPort;
 
     @Override
     public void execute(final Post post) {
-        postPort.insert(post);
+        postPort.create(post);
     }
 }
