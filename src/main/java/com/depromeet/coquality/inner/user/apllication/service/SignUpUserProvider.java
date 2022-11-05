740da87df5c7f903d8df0285b1bfbfeab4de92b2
LeeJejune <leejejune0625@gmail.com>
2022-11-05 18:18:16 +0900

    feat: 소셜 로그인 Service를 위한 Provider 구현

---
+++ src/main/java/com/depromeet/coquality/inner/user/apllication/service/SignUpUserProvider.java
@@ -0,0 +1,32 @@
+package com.depromeet.coquality.inner.user.apllication.service;
+
+import com.depromeet.coquality.inner.user.apllication.service.kakao.KaKaoSignUpService;
+import com.depromeet.coquality.inner.user.port.driving.SignUpUserUseCase;
+import com.depromeet.coquality.outer.user.entity.UserSocialType;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import java.util.HashMap;
+import java.util.Map;
+
+@RequiredArgsConstructor
+@Component
+public class SignUpUserProvider {
+    private static final Map<UserSocialType, SignUpUserUseCase> signUserServiceMap = new HashMap<>();
+
+    private final KaKaoSignUpService kaKaoAuthService;
+    //TODO 애플, 구글 확장 고려
+//    private final AppleAuthService appleAuthService;
+//    private final GoogleAuthService googleAuthService;
+
+    @PostConstruct
+    void initializeAuthServicesMap() {
+        signUserServiceMap.put(UserSocialType.KAKAO, kaKaoAuthService);
+//        authServiceMap.put(UserSocialType.APPLE, appleAuthService);
+//        authServiceMap.put(UserSocialType.GOOGLE, googleAuthService);
+    }
+    public SignUpUserUseCase getSignUpService(UserSocialType socialType) {
+        return signUserServiceMap.get(socialType);
+    }
+}
