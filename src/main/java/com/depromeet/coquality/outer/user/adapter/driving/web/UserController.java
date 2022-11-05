5e4321cd5dbb064bdd351509bed629686164d461
LeeJejune <leejejune0625@gmail.com>
2022-11-05 18:20:26 +0900

    feat: Usercontroller 구현 및 회원가입 구현

---
+++ src/main/java/com/depromeet/coquality/outer/user/adapter/driving/web/UserController.java
@@ -0,0 +1,27 @@
+package com.depromeet.coquality.outer.user.adapter.driving.web;
+
+
+import com.depromeet.coquality.inner.user.apllication.service.SignUpUserProvider;
+import com.depromeet.coquality.inner.user.port.driving.SignUpUserUseCase;
+import com.depromeet.coquality.outer.user.adapter.driving.web.dto.reqeust.SignUpRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/user")
+public class UserController {
+    private final SignUpUserProvider userProvider;
+
+    @PostMapping("/signup") // 동작확인을 위해 id 리턴시켰습니다~
+    public Long signUp(@Valid @RequestBody final SignUpRequest request){
+        final SignUpUserUseCase signUpUserUseCase = userProvider.getSignUpService(request.getSocialType());
+        final Long userId = signUpUserUseCase.execute(request.toInnerDto());
+        return userId;
+    }
+}
