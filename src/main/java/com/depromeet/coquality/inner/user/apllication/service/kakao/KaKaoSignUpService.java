236668cdf144298f2b2224503a773f0b3e2c747a
LeeJejune <leejejune0625@gmail.com>
2022-11-06 18:40:43 +0900

    feat: 중복 회원가입 불가하게 기능 구현

--- src/main/java/com/depromeet/coquality/inner/user/apllication/service/kakao/KaKaoSignUpService.java
+++ src/main/java/com/depromeet/coquality/inner/user/apllication/service/kakao/KaKaoSignUpService.java
@@ -1,6 +1,7 @@
 package com.depromeet.coquality.inner.user.apllication.service.kakao;
 
 import com.depromeet.coquality.inner.common.util.HttpHeaderUtils;
+import com.depromeet.coquality.inner.user.apllication.service.utils.SignUpUserServiceUtils;
 import com.depromeet.coquality.inner.user.domain.User;
 import com.depromeet.coquality.inner.user.port.driven.UserPort;
 import com.depromeet.coquality.inner.user.port.driving.SignUpUserUseCase;
@@ -20,10 +21,12 @@ public class KaKaoSignUpService implements SignUpUserUseCase {
     private static final UserSocialType socialType = UserSocialType.KAKAO;
     private final UserPort userPort;
     private final KaKaoAuthApiClient kakaoAuthApiCaller;
+    private final SignUpUserServiceUtils signUpUserServiceUtils;
 
     @Override
     public Long execute(final SignUpDto request) {
         final KaKaoProfileResponse response = kakaoAuthApiCaller.getProfileInfo(HttpHeaderUtils.withBearerToken(request.getToken()));
+        signUpUserServiceUtils.validateNotExistsUser(response.getId(), socialType);
         return userPort.insert(User.of(response.getId(), response.getAccount().getEmail()), socialType);
     }
 }
