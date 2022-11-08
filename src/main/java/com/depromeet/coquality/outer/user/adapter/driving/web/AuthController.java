67fa9f71c74947145929d4c4cc34feae75e6220d
LeeJejune <leejejune0625@gmail.com>
2022-11-08 17:47:33 +0900

    fix: 소셜 로그인 후 JWT 변환을 위한 AuthController 수정 및 response 수정

--- src/main/java/com/depromeet/coquality/outer/user/adapter/driving/web/AuthController.java
+++ src/main/java/com/depromeet/coquality/outer/user/adapter/driving/web/AuthController.java
@@ -5,8 +5,12 @@ import com.depromeet.coquality.inner.user.apllication.service.SignInUserProvider
 import com.depromeet.coquality.inner.user.apllication.service.SignUpUserProvider;
 import com.depromeet.coquality.inner.user.port.driving.SignInUserUseCase;
 import com.depromeet.coquality.inner.user.port.driving.SignUpUserUseCase;
+import com.depromeet.coquality.outer.jwt.JwtService;
 import com.depromeet.coquality.outer.user.adapter.driving.web.dto.reqeust.LoginRequest;
 import com.depromeet.coquality.outer.user.adapter.driving.web.dto.reqeust.SignUpRequest;
+import com.depromeet.coquality.outer.user.adapter.driving.web.dto.response.LoginResponse;
+import com.depromeet.coquality.outer.user.adapter.driving.web.dto.response.SignUpResponse;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,17 +25,20 @@ import javax.validation.Valid;
 public class AuthController {
     private final SignUpUserProvider signUpUserProvider;
     private final SignInUserProvider signInUserProvider;
+    private final JwtService jwtService;
 
     @PostMapping("/signup")
-    public Long signUp(@Valid @RequestBody final SignUpRequest request){
+    public SignUpResponse signUp(@Valid @RequestBody final SignUpRequest request){
         final SignUpUserUseCase signUpUserUseCase = signUpUserProvider.getSignUpService(request.getSocialType());
         final Long userId = signUpUserUseCase.execute(request.toInnerDto());
-        return userId;
+        final String token = jwtService.issuedToken(String.valueOf(userId), "USER", 60 * 60 * 24 * 30L);
+        return SignUpResponse.of(token, userId);
     }
     @PostMapping("/singin")
-    public Long signIn(@Valid @RequestBody final LoginRequest request){
+    public LoginResponse signIn(@Valid @RequestBody final LoginRequest request){
         final SignInUserUseCase signInUserUseCase = signInUserProvider.getSignUpService(request.getSocialType());
         final Long userId = signInUserUseCase.execute(request.toInnerDto());
-        return userId;
+        final String token = jwtService.issuedToken(String.valueOf(userId), "USER", 60 * 60 * 24 * 30L);
+        return LoginResponse.of(token, userId);
     }
 }
