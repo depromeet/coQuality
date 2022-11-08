97fd9966bbef5e0439c45c90618b592e5078accd
LeeJejune <leejejune0625@gmail.com>
2022-11-08 17:46:07 +0900

    feat: UserId 바인딩을 위한 Config 구현

---
+++ src/main/java/com/depromeet/coquality/outer/common/config/WebConfig.java
@@ -0,0 +1,28 @@
+package com.depromeet.coquality.outer.common.config;
+
+import com.depromeet.coquality.outer.interceptor.AuthInterceptor;
+import com.depromeet.coquality.outer.resolver.UserIdResolver;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@RequiredArgsConstructor
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    private final AuthInterceptor authInterceptor;
+    private final UserIdResolver userIdResolver;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authInterceptor);
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdResolver);
+    }
+}
