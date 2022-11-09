3952a7b51f7a149e2c000a0a07b96006afe9e732
wooyounggggg <souljit2@gmail.com>
2022-11-10 01:03:53 +0900

    remove: 필요없는 클래스 삭제

--- src/main/java/com/depromeet/coquality/outer/post/request/ModifyPostRequest.java
+++
@@ -1,8 +0,0 @@
-package com.depromeet.coquality.outer.post.request;
-
-import java.util.Optional;
-import javax.validation.constraints.NotBlank;
-
-public record ModifyPostRequest(Optional<@NotBlank String> title) {
-
-}
