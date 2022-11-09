262cd8e743a4cce361c75a1e0c9db4293c162a6b
wooyounggggg <souljit2@gmail.com>
2022-11-09 23:39:34 +0900

    fix: @Transactional annotation 반영

--- src/main/java/com/depromeet/coquality/inner/post/application/service/ReadPostService.java
+++ src/main/java/com/depromeet/coquality/inner/post/application/service/ReadPostService.java
@@ -3,9 +3,9 @@ package com.depromeet.coquality.inner.post.application.service;
 import com.depromeet.coquality.inner.post.domain.Post;
 import com.depromeet.coquality.inner.post.port.driven.PostPort;
 import com.depromeet.coquality.inner.post.port.driving.ReadPostUseCase;
-import javax.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -13,8 +13,9 @@ public class ReadPostService implements ReadPostUseCase {
 
     private final PostPort postPort;
 
+    @Transactional
     @Override
-    public Post execute(@Min(1) final Long id) {
+    public Post execute(final Long id) {
         return postPort.fetch(id);
     }
 }
