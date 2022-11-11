916703162ea542cf964d01e4aacf27ff10105eb9
LeeJejune <leejejune0625@gmail.com>
2022-11-11 14:56:28 +0900

    fix: 닉네임과 이메일도 받기

--- src/main/java/com/depromeet/coquality/inner/user/port/driving/dto/request/SignUpDto.java
+++ src/main/java/com/depromeet/coquality/inner/user/port/driving/dto/request/SignUpDto.java
@@ -8,12 +8,18 @@ public class SignUpDto {
     private String token;
     private String socialType;
 
-    private SignUpDto(final String token, final String socialType) {
+    private String nickname;
+
+    private String email;
+
+    private SignUpDto(final String token, final String socialType, final String nickname, final String email) {
         this.token = token;
         this.socialType = socialType;
+        this.nickname = nickname;
+        this.email = email;
     }
 
-    public static SignUpDto of(final String token, final String socialType) {
-        return new SignUpDto(token, socialType);
+    public static SignUpDto of(final String token, final String socialType, final String nickname, final String email) {
+        return new SignUpDto(token, socialType, nickname, email);
     }
 }
