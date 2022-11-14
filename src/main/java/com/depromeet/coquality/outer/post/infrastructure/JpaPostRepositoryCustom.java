904833e2d42538d2203a89e9f850ae888ec1d39b
wooyounggggg <souljit2@gmail.com>
2022-11-14 18:34:45 +0900

    refactor: 필요없는 annotation 제거

--- src/main/java/com/depromeet/coquality/outer/post/infrastructure/JpaPostRepositoryCustom.java
+++ src/main/java/com/depromeet/coquality/outer/post/infrastructure/JpaPostRepositoryCustom.java
@@ -3,9 +3,7 @@ package com.depromeet.coquality.outer.post.infrastructure;
 import com.depromeet.coquality.inner.post.vo.PostsReadInfo;
 import com.depromeet.coquality.outer.post.entity.PostEntity;
 import java.util.List;
-import org.springframework.data.repository.NoRepositoryBean;
 
-@NoRepositoryBean
 public interface JpaPostRepositoryCustom {
 
     List<PostEntity> findByPostsReadInfo(PostsReadInfo postsReadInfo);
