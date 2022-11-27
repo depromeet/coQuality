85f10c4bf98d0001b03340056a5b8a6a990f547b
LeeJejune <leejejune0625@gmail.com>
2022-11-27 17:26:55 +0900

    fix: id 필드 추가

--- src/main/java/com/depromeet/coquality/outer/comment/entity/CommentEntity.java
+++ src/main/java/com/depromeet/coquality/outer/comment/entity/CommentEntity.java
@@ -1,6 +1,10 @@
 package com.depromeet.coquality.outer.comment.entity;
 
+import com.depromeet.coquality.inner.comment.domain.Comment;
 import com.depromeet.coquality.outer.common.entity.BaseEntity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
@@ -26,10 +30,18 @@ public class CommentEntity extends BaseEntity {
     private LocalDateTime deletedAt;
 
     @Builder(builderMethodName = "factory", buildMethodName = "newInstance")
-    private CommentEntity(final Long id, final String contents, final Long userId, final Long postId) {
+    private CommentEntity(final String contents, final Long userId, final Long postId) {
         this.contents = contents;
         this.userId = userId;
         this.postId = postId;
         this.deletedAt = null;
+    }
+    public Comment toComment(){
+        return Comment.of(
+                getId(),
+                this.contents,
+                this.userId,
+                this.postId
+        );
     }
 }
