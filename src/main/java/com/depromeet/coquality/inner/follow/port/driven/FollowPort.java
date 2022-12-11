99b23d040565f76861f7274ec11bcb13c31e4241
LeeJejune <leejejune0625@gmail.com>
2022-12-11 14:45:25 +0900

    feat: 특정 유저의 팔로워 리스트 조회 Port 추가

--- src/main/java/com/depromeet/coquality/inner/follow/port/driven/FollowPort.java
+++ src/main/java/com/depromeet/coquality/inner/follow/port/driven/FollowPort.java
@@ -1,9 +1,13 @@
 package com.depromeet.coquality.inner.follow.port.driven;
 
 import com.depromeet.coquality.inner.follow.domain.Follow;
+import com.depromeet.coquality.outer.user.entity.UserEntity;
+import java.util.List;
 
 public interface FollowPort {
     void save(Follow follow);
     void delete(Long fromUserId, Long toUserId);
     Long fetchFollowCount(Long userId);
+
+    List<UserEntity> fetchFollowers(Long toUserId);
 }
