c6a02496280ecbada7249ff2b109f5809891815f
LeeJejune <leejejune0625@gmail.com>
2022-12-11 17:09:44 +0900

    feat: 북마크 수정하기 기능 Port 구현

--- src/main/java/com/depromeet/coquality/inner/bookmark/port/driven/BookmarkPort.java
+++ src/main/java/com/depromeet/coquality/inner/bookmark/port/driven/BookmarkPort.java
@@ -5,4 +5,5 @@ import com.depromeet.coquality.inner.bookmark.domain.Bookmark;
 public interface BookmarkPort {
     Long save(Bookmark bookmark);
     void delete(Bookmark bookmark);
+    void changeBookmark(Long bookmarkId, String description);
 }
