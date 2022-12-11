07d8f6bf51d7700119e822d9deab4559ad4578c8
LeeJejune <leejejune0625@gmail.com>
2022-12-11 17:09:16 +0900

    feat: 북마크 수정하기 기능 Controller 구현

--- src/main/java/com/depromeet/coquality/outer/bookmark/adapter/driving/web/BookmarkController.java
+++ src/main/java/com/depromeet/coquality/outer/bookmark/adapter/driving/web/BookmarkController.java
@@ -2,13 +2,18 @@ package com.depromeet.coquality.outer.bookmark.adapter.driving.web;
 
 import com.depromeet.coquality.inner.bookmark.port.driving.CreateBookmarkUseCase;
 import com.depromeet.coquality.inner.bookmark.port.driving.DeleteBookmarkUseCase;
+import com.depromeet.coquality.inner.bookmark.port.driving.PutBookmarkUseCase;
+import com.depromeet.coquality.outer.bookmark.adapter.driving.web.request.BookmarkRequest;
 import com.depromeet.coquality.outer.common.vo.ApiResponse;
 import com.depromeet.coquality.outer.interceptor.Auth;
 import com.depromeet.coquality.outer.resolver.UserId;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,6 +24,7 @@ public class BookmarkController {
 
     private final CreateBookmarkUseCase createBookmarkUseCase;
     private final DeleteBookmarkUseCase deleteBookmarkUseCase;
+    private final PutBookmarkUseCase putBookmarkUseCase;
 
     @Auth
     @PostMapping("/{id}")
@@ -31,4 +37,10 @@ public class BookmarkController {
     public void deleteBookmark(@UserId final Long userId, @PathVariable("id") final Long postId){
         deleteBookmarkUseCase.execute(userId, postId);
     }
+
+    @PutMapping("/{id}")
+    public void putBookmark(@PathVariable("id") final Long bookmarkId, @RequestBody @Valid final BookmarkRequest request){
+        putBookmarkUseCase.execute(bookmarkId, request.toBookmarkDto());
+    }
+
 }
