772981213dbe1555e3b9b65f66accc2902b6705d
LeeJejune <leejejune0625@gmail.com>
2022-12-11 16:55:30 +0900

    feat: 북마크 취소 기능 Conroller 구현

--- src/main/java/com/depromeet/coquality/outer/bookmark/adapter/driving/web/BookmarkController.java
+++ src/main/java/com/depromeet/coquality/outer/bookmark/adapter/driving/web/BookmarkController.java
@@ -1,9 +1,12 @@
 package com.depromeet.coquality.outer.bookmark.adapter.driving.web;
 
 import com.depromeet.coquality.inner.bookmark.port.driving.CreateBookmarkUseCase;
+import com.depromeet.coquality.inner.bookmark.port.driving.DeleteBookmarkUseCase;
+import com.depromeet.coquality.outer.common.vo.ApiResponse;
 import com.depromeet.coquality.outer.interceptor.Auth;
 import com.depromeet.coquality.outer.resolver.UserId;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +18,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class BookmarkController {
 
     private final CreateBookmarkUseCase createBookmarkUseCase;
+    private final DeleteBookmarkUseCase deleteBookmarkUseCase;
 
     @Auth
     @PostMapping("/{id}")
     public ApiResponse createBookmark(@UserId final Long userId, @PathVariable("id") final Long postId){
         return ApiResponse.success(createBookmarkUseCase.execute(userId, postId));
+    }
+
+    @Auth
+    @DeleteMapping("/{id}")
+    public void deleteBookmark(@UserId final Long userId, @PathVariable("id") final Long postId){
+        deleteBookmarkUseCase.execute(userId, postId);
     }
 }
