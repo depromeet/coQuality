6d487fabfb98037ab19c5ae8bc4704a6ab3f4ef5
LeeJejune <leejejune0625@gmail.com>
2022-12-31 18:40:08 +0900

    fix: test

--- src/test/java/com/depromeet/coquality/CoQualityApplicationTests.java
+++ src/test/java/com/depromeet/coquality/CoQualityApplicationTests.java
@@ -1,9 +1,8 @@
 package com.depromeet.coquality;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class CoQualityApplicationTests {
 
     @Test
