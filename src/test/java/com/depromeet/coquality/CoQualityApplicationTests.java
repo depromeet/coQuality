ad9618b6730f99d27a483c0f61890eb041d7fa77
LeeJejune <leejejune0625@gmail.com>
2022-12-31 18:11:41 +0900

    fix: springbootTest

--- src/test/java/com/depromeet/coquality/CoQualityApplicationTests.java
+++ src/test/java/com/depromeet/coquality/CoQualityApplicationTests.java
@@ -1,7 +1,9 @@
 package com.depromeet.coquality;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
+@SpringBootTest(properties = "spring.profiles.active:prod")
 class CoQualityApplicationTests {
 
     @Test
