9bc43050ad5d50bd370166a6e3d5e0b7a5832b26
wooyounggggg <souljit2@gmail.com>
2022-12-31 18:15:26 +0900

    feat: test에 application yml 추가

--- src/test/java/com/depromeet/coquality/CoQualityApplicationTests.java
+++ src/test/java/com/depromeet/coquality/CoQualityApplicationTests.java
@@ -3,7 +3,7 @@ package com.depromeet.coquality;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(properties = "spring.profiles.active:prod")
+@SpringBootTest
 class CoQualityApplicationTests {
 
     @Test
