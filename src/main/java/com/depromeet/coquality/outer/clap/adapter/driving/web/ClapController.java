94ea85e7573cabea85bf5d86b2482bba6b84d52e
LeeJejune <leejejune0625@gmail.com>
2023-01-04 22:39:13 +0900

    fix: 잘못된 로직 수정

--- src/main/java/com/depromeet/coquality/outer/clap/adapter/driving/web/ClapController.java
+++ src/main/java/com/depromeet/coquality/outer/clap/adapter/driving/web/ClapController.java
@@ -1,7 +1,6 @@
 package com.depromeet.coquality.outer.clap.adapter.driving.web;
 
 import com.depromeet.coquality.inner.clap.port.driving.ClapUseCase;
-import com.depromeet.coquality.inner.rank.application.service.RankUseCaseService;
 import com.depromeet.coquality.outer.interceptor.Auth;
 import com.depromeet.coquality.outer.resolver.UserId;
 import lombok.RequiredArgsConstructor;
@@ -16,12 +15,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ClapController {
 
     private final ClapUseCase clapUseCase;
-    private final RankUseCaseService rankUseCaseService;
 
     @PostMapping("/posts/{postId}")
     @Auth
     public void doClap(@PathVariable final Long postId, @UserId Long userId) {
-        rankUseCaseService.achieveSpecialEditor(userId);
         clapUseCase.execute(postId, userId);
     }
 }
