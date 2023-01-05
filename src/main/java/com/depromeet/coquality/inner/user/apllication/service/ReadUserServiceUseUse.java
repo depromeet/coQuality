a1c65224cea412be9c52a72eaa07ec093552c398
LeeJejune <leejejune0625@gmail.com>
2023-01-05 18:06:07 +0900

    fix: naming 수정

--- src/main/java/com/depromeet/coquality/inner/user/apllication/service/ReadUserServiceUseUse.java
+++ src/main/java/com/depromeet/coquality/inner/user/apllication/service/ReadUserServiceUseUse.java
@@ -2,12 +2,8 @@ package com.depromeet.coquality.inner.user.apllication.service;
 
 import com.depromeet.coquality.inner.rank.application.service.RankUseCaseService;
 import com.depromeet.coquality.inner.user.port.driven.UserPort;
-<<<<<<< HEAD:src/main/java/com/depromeet/coquality/inner/user/apllication/service/ReadUserService.java
-import com.depromeet.coquality.inner.user.port.driving.ReadUserUserCase;
-import com.depromeet.coquality.inner.user.vo.UserResponse;
-=======
 import com.depromeet.coquality.inner.user.port.driving.ReadUserUseCase;
->>>>>>> main:src/main/java/com/depromeet/coquality/inner/user/apllication/service/ReadUserServiceUse.java
+import com.depromeet.coquality.inner.user.vo.UserResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,7 +11,7 @@ import org.springframework.transaction.annotation.Transactional;
 @RequiredArgsConstructor
 @Transactional
 @Service
-public class ReadUserServiceUse implements ReadUserUseCase {
+public class ReadUserServiceUseUse implements ReadUserUseCase {
     private final UserPort userPort;
     private final RankUseCaseService rankUseCaseService;
 
--- src/main/java/com/depromeet/coquality/inner/user/port/driving/ReadUserUseCase.java
+++ src/main/java/com/depromeet/coquality/inner/user/port/driving/ReadUserUseCase.java
@@ -2,6 +2,6 @@ package com.depromeet.coquality.inner.user.port.driving;
 
 import com.depromeet.coquality.inner.user.vo.UserResponse;
 
-public interface ReadUserUserCase {
+public interface ReadUserUseCase {
     UserResponse execute(final Long userId);
 }
