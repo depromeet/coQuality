43e06020d821070ae19ef20ba0625536d9c0540b
LeeJejune <leejejune0625@gmail.com>
2023-01-07 15:30:17 +0900

    fix: 북마크 여부 네이밍 변경

--- src/main/java/com/depromeet/coquality/inner/bookmark/vo/BookmarkConditionResponse.java
+++ src/main/java/com/depromeet/coquality/inner/bookmark/vo/BookmarkConditionResponse.java
@@ -4,12 +4,12 @@ import lombok.Getter;
 
 @Getter
 public class BookmarkConditionResponse {
-    private boolean condition;
+    private boolean bookmarkYn;
 
-    private BookmarkConditionResponse(final boolean condition) {
-        this.condition = condition;
+    private BookmarkConditionResponse(final boolean bookmarkYn) {
+        this.bookmarkYn = bookmarkYn;
     }
-    public static BookmarkConditionResponse of(final boolean condition){
-        return new BookmarkConditionResponse(condition);
+    public static BookmarkConditionResponse of(final boolean bookmarkYn){
+        return new BookmarkConditionResponse(bookmarkYn);
     }
 }
